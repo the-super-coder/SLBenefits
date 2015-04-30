e3710d56c2db3ef60934c5fff55c4a720a7ac651
aamir-poswal <aamir.pucit@gmail.com>
2015-04-30 08:43:02 +0500

    initial project set up

---
+++ SLBenefits/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SLBenefits
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
